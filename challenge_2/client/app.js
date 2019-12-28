db3f0c53c8404659eebecb6b95e46d3a650a307f
omarbara <omarbara@ymail.com>
2019-12-28 18:40:11 +0100

    chalenge2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,37 @@
+const server = require('../server.js')
+console.log('app')
+
+// function jsonParser(jsonObj) {
+// 	var results = [];
+// 	for (var key in jsonObj) {
+// 		if (!Array.isArray(jsonObj[key])) {
+// 			results.push(jsonObj[key]);
+// 		}
+// 	}
+// 	if (jsonObj.children) {
+// 		for (var i = 0; i < jsonObj.children.length; i++) {
+// 			var childResult = jsonParser(jsonObj.children[i]);
+// 			results = results.concat(childResult);
+// 		}
+// 	}
+//   results = 'firstName,lastName,county,city,role,sales \n'+results.join(',');
+//   return results
+// }
+
+var createJson = function(e) {
+  e.preventDefault();
+  console.log('createJson----------- ')
+  var formData = JSON.stringify($("#myform").serializeArray());
 
 
+  $.ajax({
+    type: "POST",
+    url: "/",
+    data: formData,
+    success: function(){
+      console.log('Ajax was sent')
+    },
+    dataType: "json",
+    contentType : "application/json"
+  });
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,42 @@
+const express = require('express')
+const app = express()
+var path = require('path');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+const port = 3000
+const ObjectsToCsv = require('objects-to-csv');
 
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+app.post('/',function(req,res) {
+  // console.log(JSON.stringify(req.body))
+  var recived = req.body
+  console.log(recived)
+
+  function jsonParser(jsonObj) {
+    var results = [];
+    for (var key in jsonObj) {
+      if (!Array.isArray(jsonObj[key])) {
+        results.push(jsonObj[key]);
+      }
+    }
+    if (jsonObj.children) {
+      for (var i = 0; i < jsonObj.children.length; i++) {
+        var childResult = jsonParser(jsonObj.children[i]);
+        results = results.concat(childResult);
+      }
+    }
+    results = 'firstName,lastName,county,city,role,sales \n'+results.join(',');
+    return results
+  }
+
+  console.log(JSON.parse(jsonParser(recived)))
+
+  res.send(JSON.parse(jsonParser(recived)))
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
 
