d5e882e700f49d27c1cbea0e9707b08cc91fda42
OmarBara <omarbara00@yahoo.com>
2019-12-29 12:58:55 +0100

    Challenge: CSV Report Generator

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -13,30 +13,33 @@ app.get('/',function(req,res) {
 });
 app.post('/',function(req,res) {
   // console.log(JSON.stringify(req.body))
-  var recived = req.body
+  var recived = req.body.data
   console.log(recived)
 
-  function jsonParser(jsonObj) {
+  function jsonParser(msg) {
     var results = [];
-    for (var key in jsonObj) {
-      if (!Array.isArray(jsonObj[key])) {
-        results.push(jsonObj[key]);
+    for (var key in msg) {
+      if (!Array.isArray(msg[key])) {
+        results.push(msg[key]);
       }
     }
-    if (jsonObj.children) {
-      for (var i = 0; i < jsonObj.children.length; i++) {
-        var childResult = jsonParser(jsonObj.children[i]);
+    if (msg.children) {
+      for (var i = 0; i < msg.children.length; i++) {
+        var childResult = jsonParser(msg.children[i]);
         results = results.concat(childResult);
       }
     }
     results = 'firstName,lastName,county,city,role,sales \n'+results.join(',');
     return results
   }
+  console.log('------------------------first')
+  var paesedRecived = JSON.parse(recived)
+  console.log(paesedRecived)
+  console.log('------------------------////////////////////')
+  // console.log(JSON.parse(jsonParser(recived)))
 
-  console.log(JSON.parse(jsonParser(recived)))
-
-  res.send(JSON.parse(jsonParser(recived)))
+  // res.send(JSON.parse(jsonParser(recived)))
+  res.send(jsonParser(paesedRecived))
 });
-
 app.listen(port, () => console.log(`app listening on port ${port}!`))
 
